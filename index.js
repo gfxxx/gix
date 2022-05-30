744957d15d3040c125f03bb77ac688ee32f0661c
Guiroux <fassotguillaume@gmail.com>
2022-05-30 17:01:51 +0200

    First commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { exec } from 'child_process';
+
 const command = process.argv.slice(2);
 
 // Git Commands
@@ -8,19 +10,23 @@ const push = 'git push';
 const init = () => {
   console.log({command})
   try {
-    // Get origin and throw error if no origin provided
-    const originIndex = command.findIndex(e => e === '-o');
-    console.log({originIndex})
-    if(originIndex === -1) throw new Error('An origin is required use -o to set origin');
-    const origin = command[originIndex + 1];
-    // Get branch and set it to master if none provided
-    const branchIndex = command.findIndex(e => e === '-b');
-    const branch = branchIndex !== -1 ? command[branchIndex + 1] : 'master';
+    const cmds = []
+    cmds.push('git init');
+    cmds.push('git add .');
     // Get commit message and set it to "First commit" if none is provided
     const messageIndex = command.findIndex(e => e === '-m');
     const message = messageIndex !== -1 ? command[messageIndex + 1]: 'First commit';
-  
-    return `git init && git add . && git commit -m "${message}" && git remote add origin ${origin}`
+    cmds.push(`git commit -m "${message}"`);
+    // Get origin and throw error if no origin provided
+    const originIndex = command.findIndex(e => e === '-o');
+    if(originIndex === -1) throw new Error('An origin is required use -o to set origin');
+    const origin = command[originIndex + 1];
+    cmds.push(`git remote add origin ${origin}`)
+    // Get branch and set it to master if none provided
+    const branchIndex = command.findIndex(e => e === '-b');
+    const branch = branchIndex !== -1 ? command[branchIndex + 1] : 'master';
+    cmds.push(`git push origin ${branch}`)
+    return cmds
   } catch (error) {
     console.error(error);
   }
@@ -57,11 +63,13 @@ const execut = () => {
     default:
       console.log('unknown command')
   }
-  require('child_process').exec(com, (err, stdout, stderr) => {
-    if(err) console.error(err)
-    if(stdout) console.log(stdout)
-    if(stderr) console.error(stderr)
-  })
+  for(let c of com){
+    exec(c, (err, stdout, stderr) => {
+      if(err) console.error(err)
+      if(stdout) console.log(stdout)
+      if(stderr) console.error(stderr)
+    })
+  }
 }
 
 execut();
